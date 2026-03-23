bec625ca9b770b4f7ae99c49904fad5add1f0016
sofina-dev <81674971+sofina-dev@users.noreply.github.com>
2026-03-24 03:41:49 +0700

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const path = url.pathname.replace('/api', '');
 
-  const GAS_BASE = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_ID/exec";
+  const GAS_BASE = "https://script.google.com/macros/s/AKfycbzijoRa_2vZ3y5F3KzVkxJxgO14MsSyfik3boH1cqLH_Bc5bz2ChNk5iPXj_WycksuF1w/exec";
 
   const target = GAS_BASE + path + url.search;
   const res = await fetch(target);
