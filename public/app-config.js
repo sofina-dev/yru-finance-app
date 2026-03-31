3a352fa420c380fa82f64eb7a7230283608c0f7b
zensor-dev <91815072+zensor-dev@users.noreply.github.com>
2026-03-31 16:22:35 +0700

    Update app-config.js
    v.11

--- public/app-config.js
+++ public/app-config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  GAS_BASE: "https://script.google.com/macros/s/AKfycbz8-s_ZguywHelbx0zpzYsADFFO9xvb8QvhkdHX4YJO3iRzPNTSDM8wg11CD-68H-rp4g/exec"
+  GAS_BASE: "https://script.google.com/macros/s/AKfycbzdC7uIoIqAPDPwCahaSKhu704TWTCYEJrONJooU3kEWtEY-2bRqjBsYP8c_o9wqino/exec"
 };
